23a45340457ab00712ad66c536b85e3b9909b43d
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-02-23 18:53:47 +0300

    Add files via upload

---
+++ date.py
@@ -0,0 +1,18 @@
+import datetime
+
+tt= datetime.date(1978, 5 ,12)
+ttt = datetime.time(00 ,15, 18)
+
+print(tt)
+print(tt.year)
+print(tt.month)
+print(tt.day)
+
+
+print(ttt)
+print(ttt.hour)
+print(ttt.minute)
+print(ttt.second)
+
+tttt = datetime.datetime(1978, 12, 5, 0, 15, 00)
+print(tttt)
